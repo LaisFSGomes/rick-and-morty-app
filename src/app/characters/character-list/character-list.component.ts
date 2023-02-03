47134fee8d90693cb661aef557bc25df3c15ab5f
LaisFSGomes <laisgomes@alu.ufc.br>
2023-02-02 21:17:08 -0300

    chore: paginação e responsividade da página de listar personagens feita

--- src/app/characters/character-list/character-list.component.ts
+++ src/app/characters/character-list/character-list.component.ts
@@ -10,17 +10,47 @@ import { Component } from '@angular/core';
 export class CharacterListComponent {
   searchCharacterName: string = '';
   currentChacactersPage: CharacterType[] = [];
+  totalPages: number = 0;
+  currentPage: number = 0;
   constructor (private service: ApiService) { }
 
 
   ngOnInit(): void {
     this.service.listar().subscribe((data) => {
       this.currentChacactersPage = data.results;
+      this.totalPages = data.info.pages;
+      this.currentPage = 1;
     });
   }
   onClickSearch() {
     this.service.filterCharacters(`name=${this.searchCharacterName}`).subscribe((data) => {
       this.currentChacactersPage = data.results;
+      this.totalPages = data.info.pages;
+      this.currentPage = 1;
+    });
+  }
+  onClickPreviousPage() {
+    this.service.listarPaginacao(this.currentPage - 1).subscribe((data) => {
+      this.currentChacactersPage = data.results;
+      this.currentPage = this.currentPage - 1;
+    });
+  }
+  onClickNextPage() {
+    this.service.listarPaginacao(this.currentPage + 1).subscribe((data) => {
+      this.currentChacactersPage = data.results;
+      this.currentPage = this.currentPage + 1;
+    });
+  }
+  onClickFirstPage() {
+    this.service.listarPaginacao(1).subscribe((data) => {
+      this.currentChacactersPage = data.results;
+      this.currentPage = 1;
+    });
+  }
+  onClickLastPage() {
+    this.service.listarPaginacao(this.totalPages).subscribe((data) => {
+      this.currentChacactersPage = data.results;
+      this.currentPage = this.totalPages;
     });
   }
 }
